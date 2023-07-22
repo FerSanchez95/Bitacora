aee73f0fd31a5dc75e415dac06c5843aa2f9360d
fernando <fersanchezz95@gmail.com>
2023-07-22 16:57:58 -0300

    Función de apertura de base de datos agregada y funcionando.

--- flaskMian.py
+++ flaskMian.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, g, flash
+from flask import Flask, render_template, redirect, url_for, request, g
 #from flaskwebgui import FlaskUI
 from tkinter import filedialog
 from datetime import datetime
@@ -8,16 +8,18 @@ from code_search_test import code_selector as cs
 
 app = Flask(__name__)
 app.secret_key = '54654*adqweqdasdsd_ad32432?#'
-#ui = FlaskUI(app)
+#ui = FlaskUI(app) # Para ejecutar la app como una GUI.
 transferList = []
+
+
 # Funciones fuera de Flask ======================
 
 # Inicialización de base de datos.
 # Voy a usar la plantilla por defecto de la documentación de Flask.
-def get_db():
+def get_db(path_to_db):
     db = getattr(g, "_database", None)
     if 'db' not in g:
-        db = g.db = sqlite3.connect('test_data.db') # dbLog.db
+        db = g.db = sqlite3.connect(path_to_db) # dbLog.db; 'test_data.db'
         g.db.row_factory = sqlite3.Row
     return db
 
@@ -39,22 +41,23 @@ def init_db():
     return db
 
 # Comprobador de existencia. se fija si la base de datos existe, si lo hace no utiliza 'init_db'
-def exist_db():
-    ruta = os.getcwd() + "/test_data.db" # /dbLog.db
-    if not os.path.isfile(ruta):
-        db = init_db()
-        flash('Creando DB: dbLog.db')
+def exist_db(filename_path):
+    #ruta = os.getcwd() + "/test_data.db" # /dbLog.db
+
+    if not os.path.isfile(filename_path):
+        recently_created_db = init_db()
         print('se activó init_db')
-        return db
+        return recently_created_db
     else: 
-        db = get_db()
-        flash('Cargando DB: dbLog.db')
+        already_existent_db = get_db(filename_path)
         print('se activo get_db')
-        return db
+       # print(f'En exist_db se utiliza la ruta: {db_filename}')
+        return already_existent_db
               
 # Formato de mensaje y guardado en DB.
 def custom_list(message):
-    transfer_db = exist_db()
+    print(filename)
+    transfer_db = exist_db(filename)
     time_now = datetime.now()
     dateKey = time_now.strftime("%Y-%m-%d") #%d-%m-%Y 
     timeKey = time_now.strftime("%H:%M:%S")
@@ -66,12 +69,12 @@ def custom_list(message):
     transferList.append(comment)
     transfer_db.close()
 
-    return reversed(transferList) 
+    return reversed(transferList) #La función 'reversed' se usa para que en la página se muestre la última entrada primero.
 
 #================================================
 # Funciones de Flask ============================
 #   -INDEX-
-@app.route("/", methods=['POST', 'GET']) #¿Qué son los decoradores?
+@app.route("/", methods=['POST', 'GET']) 
 def index():
 
     if request.method == 'POST':
@@ -82,14 +85,13 @@ def index():
         return render_template('index.html', item_list = list_out)
     else:
         error = 'mensaje d error 1'
-        flash(error)
         return render_template('index.html')
 
 #   -SEARCH-
 @app.route("/search", methods=['POST', 'GET'])
 def search():
     if request.method == 'POST':
-        db = exist_db()
+        db = exist_db(filename)
         cursor_db = db.cursor()
         Param_F_i = request.form['fecha_desde']
         Param_F_f = request.form['fecha_hasta']
@@ -109,32 +111,26 @@ def search():
         select_from_db = cursor_db.execute(sql_query, returned_list) # Ejecuto la sentencia SQL
         info_db = select_from_db.fetchall() 
 
-        #print(f'\n_ La lista devuelta desde el generador de código es:\n {returned_list}') #for debug
-        #print(f'\n_ La sentencia devuelta desde el generador de código es:\n {sql_query}\n') #for debug
-        #print(info_db) #for debug
+        print(f'\n_ La lista devuelta desde el generador de código es:\n {returned_list}') #for debug
+        print(f'\n_ La sentencia devuelta desde el generador de código es:\n {sql_query}\n') #for debug
+        print(info_db) #for debug
         db.close()
         return render_template('search_results.html', items = info_db)
     else:
         return render_template('search.html', data=None)
     
 #   -OPEN-
-@app.route("/open", methods=['GET'])
+@app.route("/open", methods=['POST', 'GET'])
 def open():
+    global filename
     filename = filedialog.askopenfilename()
     print(filename)
-    # 'filename' devuelve la ruta del archivo seleccionado.
-    # Puedo abrir ese archivo con sql e intentar imprimirlo 
-    # en la 'textarea' que muestra los logs anteriores.
-    # Importar la librería 'datetime' y guardarla en el 
-    # comentario añadido en el JSON.
-    # Guardar todos lo comentado en el textarea de salida.
-    # Puedo generar un único archivo DB para usar siempre
-    # en el mismo directorio.
-
     return redirect(url_for('index'))
+
 #   -SAVE-
 @app.route("/save", methods=['GET'])
 def save():
+    #filedialog.asksaveasfilename()
     pass
 # ================================================
 
