ad38f3d2d2a9ca9921b2a3fee4ce76e8424a2ec0
FerSanchez95 <fersanchezz95@gmail.com>
2025-03-16 16:10:05 -0300

    Se agregan DataAnnotations a la Password

--- Helpers/Mensajes.cs
+++ Helpers/Mensajes.cs
@@ -7,5 +7,9 @@
 		public const string RegistroInvalido = "No se pudo registrar el usuario.";
 		public const string InicioInvalido = "Error al iniciar sesión.";
 		public const string InicioExitoso = "¡Inicia de sesión exitoso!";
+		public const string LongitudContraseña = "La contraseña debe tener entre 6 y 30 caracteres.";
+		public const string CaracteresContraseña = "La contraseña debe contener mayúsculas, minúsculas, números y caracteres especiales.";
+		public const string confirmacionRequerida = "Ingrese nuevamente la contraseña.";
+		public const string CantidadDeCaracteres = "El nombre de usuario debe tener entre 2 y 25 caracteres.";
 	}
 }
--- Models/ViewModels/RegistroViewModel.cs
+++ Models/ViewModels/RegistroViewModel.cs
@@ -1,18 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Bitacora.Helpers;
 
 namespace Bitacora.Models.ViewModels
 {
 	public class RegistroViewModel
 	{
-		[Required, StringLength(25, MinimumLength = 2)]
+		[Required, StringLength(25, MinimumLength = 2, ErrorMessage = Mensajes.CantidadDeCaracteres)]
 		[Display(Name = "Nombre de usuario")]
 		public string NombreUsuario { get; set;}
 
 		[Required, DataType(DataType.Password)]
 		[Display(Name = "Contraseña")]
+		[StringLength(30, MinimumLength = 8, ErrorMessage = Mensajes.LongitudContraseña)]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+			ErrorMessage = Mensajes.CaracteresContraseña)]
 		public string Password { get; set; }
 
-		[Required, Compare("Password")]
+		[Required(ErrorMessage = Mensajes.confirmacionRequerida), Compare("Password")]
 		[Display(Name = "Confirmar contraseña")]
 		public string ConfirmedPassword { get; set; }
 	}
