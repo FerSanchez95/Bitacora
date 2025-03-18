4fb9aec64fbfaa863a5ba6061965e9f25c493479
FerSanchez95 <fersanchezz95@gmail.com>
2025-03-17 21:07:28 -0300

    [Fix] Se cambian los nombres que contienen acentos de las columnas de la tabla de Posts.

--- Migrations/20250318000103_Fix.Nombre.Propiedades.Post.Designer.cs
+++ Migrations/20250318000103_Fix.Nombre.Propiedades.Post.Designer.cs
@@ -4,6 +4,7 @@ using Bitacora.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bitacora.Migrations
 {
     [DbContext(typeof(BitacoraDb))]
-    partial class BitacoraDbModelSnapshot : ModelSnapshot
+    [Migration("20250318000103_Fix.Nombre.Propiedades.Post")]
+    partial class FixNombrePropiedadesPost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250318000103_Fix.Nombre.Propiedades.Post.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bitacora.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNombrePropiedadesPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HoraDeCreación",
+                table: "Posts",
+                newName: "HoraDeCreacion");
+
+            migrationBuilder.RenameColumn(
+                name: "FechaDeCreación",
+                table: "Posts",
+                newName: "FechaDeCreacion");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HoraDeCreacion",
+                table: "Posts",
+                newName: "HoraDeCreación");
+
+            migrationBuilder.RenameColumn(
+                name: "FechaDeCreacion",
+                table: "Posts",
+                newName: "FechaDeCreación");
+        }
+    }
+}
